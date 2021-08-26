59bc2dcfb6faf6fb1fd984bd64a47ac18803916b
Justin Anthony <justincanthony@gmail.com>
2021-08-26 15:22:34 -0600

    Create movie component

---
+++ src/Movies.js
@@ -0,0 +1,14 @@
+import './Movies.css';
+import MovieCard from './MovieCard';
+
+const Movies = ({ movies }) => {
+  const movieCard = movies.map((movie) => {
+    return (
+      <MovieCard
+        img={movie.poster_path}
+        title={movie.title}
+        rating={movie.average_rating}
+      />
+    );
+  });
+};
