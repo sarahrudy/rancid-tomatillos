535139999e1ed055ffde43b96f5c9ef3fc58d65b
Sarah <sarahrudy@gmail.com>
2021-08-26 17:05:04 -0600

    Add contents to Nav.js component

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import './Nav.css';
+
+const Nav = (props) => {
+  return (
+    <header className="navigation-bar">
+      <h1>RANCID TOMATILLOS</h1>
+      <input type="search" placeholder="search movies" />
+    </header>
+  )
+}
+
+export default Nav
