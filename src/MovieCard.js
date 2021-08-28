b3062dce4376adfa7a58d0a7e79f48d5ded26f0a
Justin Anthony <justincanthony@gmail.com>
2021-08-28 10:50:38 -0600

    Add css to view movies

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,7 +3,7 @@ import './MovieCard.css';
 const MovieCard = ({ img, title, rating }) => {
   return (
     <article className="movie-card">
-      <img src={img} alt="movie poster" />
+      <img className="movie-poster" src={img} alt="movie poster" />
       <h3>{title}</h3>
       <p>{rating}</p>
     </article>
