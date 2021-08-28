74b97df44880e24d337a116c49b4aa9cc8470ff8
Sarah <sarahrudy@gmail.com>
2021-08-28 12:24:57 -0600

    Initial class component setup

---
+++ src/MovieDetails.js
@@ -0,0 +1,17 @@
+import './MovieDetails.css';
+import React, { Component } from 'react';
+
+class MovieDetails extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movie: {},
+      error: '',
+    }
+  }
+
+
+
+}
+
+export default MovieDetails
