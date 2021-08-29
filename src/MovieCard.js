63eda26bb8d6c9dc07ed668c2e9869d99288cd89
Justin Anthony <justincanthony@gmail.com>
2021-08-29 15:45:25 -0600

    Round movie rating

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -5,7 +5,7 @@ const MovieCard = ({ img, title, rating }) => {
     <article className="movie-card">
       <img className="movie-poster" src={img} alt="movie poster" />
       <h3>{title}</h3>
-      <p>{rating}</p>
+      <p>{Math.floor(rating)} out of 10</p>
     </article>
   );
 };
