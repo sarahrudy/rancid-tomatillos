67e0e60028bbc11c6e5e17d22a89e4c91fcfaf08
Justin Anthony <justincanthony@gmail.com>
2021-08-29 16:28:43 -0600

    Comment out function

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -32,55 +32,25 @@ class MovieDetails extends Component {
       );
     }
 
-    if (this.state.movie.title) {
-      return (
-        <section className="single-movie-container">
-          <img
-            className="banner-photo"
-            src={this.state.movie.backdrop_path}
-            alt={`Background banner for ${this.state.movie.title}`}
-          ></img>
-          <div className="info">
-            <img
-              className="movie-poster"
-              src={this.state.movie.poster_path}
-              alt={`Poster for ${this.state.movie.title}`}
-            ></img>
-            <div className="title-container">
-              <h3 className="movie-title">{this.state.movie.title}</h3>
-              <p className="release">Release Date</p>
-              <p className="movie-release">
-                {new Date(this.state.movie.release_date).toLocaleDateString()}
-              </p>
-              <p className="rating">Average rating</p>
-              <p className="movie-rating">
-                {this.state.movie.average_rating.toFixed(2)}
-              </p>
-            </div>
-            <div className="details-container">
-              <p className="overview">Overview</p>
-              <p className="movie-overview">{this.state.movie.overview}</p>
-              <p className="genres">Genres</p>
-              <p className="movie-genres">
-                {this.state.movie.genres.join(' | ')}
-              </p>
-              <p className="budget">Budget</p>
-              <p className="movie-budget">
-                ${this.state.movie.budget.toLocaleString()}
-              </p>
-              <p className="revenue">Revenue</p>
-              <p className="movie-revenue">
-                ${this.state.movie.revenue.toLocaleString()}
-              </p>
-              <p className="runtime">Runtime</p>
-              <p className="movie-runtime">
-                {this.state.movie.runtime} minutes
-              </p>
-            </div>
-          </div>
-        </section>
-      );
-    }
+    // if (this.state.movie.title) {
+    // return (
+    // ----------single movie detail elements displayed here --------
+    // <section className="single-movie-container">
+    //   <img
+    //     className="banner-photo"
+    //     src={this.state.movie.backdrop_path}
+    //     alt={`Background banner for ${this.state.movie.title}`}
+    //   ></img>
+    //   <div className="info">
+    //     <img
+    //       className="movie-poster"
+    //       src={this.state.movie.poster_path}
+    //       alt={`Poster for ${this.state.movie.title}`}
+    //     ></img>
+    //   </div>
+    // </section>
+    // );
+    // }
   }
 }
 
