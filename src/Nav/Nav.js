14b3fade45a0b21f327728be4fa9e825f22d6273
Sarah <sarahrudy@gmail.com>
2021-09-01 17:04:05 -0600

    Remove search bar and add home navigation

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,11 +1,13 @@
 import './Nav.css';
+import { NavLink } from 'react-router-dom';
 
 const Nav = (props) => {
   return (
     <header className="navigation-bar">
       <h1>RANCID TOMATILLOS</h1>
-      <label for="searchBar"><strong>SEARCH MOVIES </strong></label>
-      <input type="search" placeholder="type here" id="searchBar" name="search-bar" />
+      <NavLink to="/" className="back">
+        Home
+      </NavLink>
     </header>
   )
 }
