2aa8298a7aef837b9af7af5ed6871dbca3a101b6
Sarah <sarahrudy@gmail.com>
2021-09-01 17:27:21 -0600

    Remove commented out code

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 import { getSingleMovie } from '../apiCalls';
 
 const MovieDetails = ({ id }) => {
-  // put single movie details function api call 
-
   const [currentMovie, setCurrentMovie ] = useState({})
   const [error, setError] = useState('')
 
@@ -29,42 +27,5 @@ const MovieDetails = ({ id }) => {
     </section>
   )
 }
-
-// dummy data from API
-// { "movie": { 
-//   id: 1, 
-//   title: "Movie Title", 
-//   poster_path: "someURL", 
-//   backdrop_path: "someURL", 
-//   release_date: "2019-12-04", 
-//   overview: "Some overview", 
-//   average_rating: 6, 
-//   genres: [
-//     { id: 18, 
-//       name: "Drama" }], 
-//   budget: 63000000, 
-//   revenue: 100853753, 
-//   runtime: 139, 
-//   tagline: "Movie Tagline" } }
-
-
-// const MovieDetails = (props) => {
-//   return (
-//     <section className="single-movie-container">
-//       <img
-//         className="banner-photo"
-//         src={props.movie.backdrop_path}
-//         alt={`Background banner for ${props.movie.title}`}
-//       ></img>
-//       <div className="info">
-//         <img
-//           className="movie-poster"
-//           src={props.movie.poster_path}
-//           alt={`Poster for ${props.movie.title}`}
-//         ></img>
-//       </div>
-//     </section>
-//   );
-// };
 
 export default MovieDetails;
