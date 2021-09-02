9ea4d46cab1fbe3b3c43d8bec0dd3acb5d9f5d3a
Justin Anthony <justincanthony@gmail.com>
2021-09-01 19:55:04 -0600

    Fix back button

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -4,28 +4,39 @@ import { Link } from 'react-router-dom';
 import { getSingleMovie } from '../apiCalls';
 
 const MovieDetails = ({ id }) => {
-  const [currentMovie, setCurrentMovie ] = useState({})
-  const [error, setError] = useState('')
+  const [currentMovie, setCurrentMovie] = useState({});
+  const [error, setError] = useState('');
 
   useEffect(() => {
     getSingleMovie(id)
       .then((movie) => setCurrentMovie(movie.movie))
-      .catch((error) => setError(error.message))
-  }, [id])
+      .catch((error) => setError(error.message));
+  }, [id]);
 
   return (
     <section className="single-movie-container">
       <h1>{currentMovie.title}</h1>
-      <img src={currentMovie.poster_path} alt={`Poster for ${currentMovie.title}`}/>
+      <img
+        src={currentMovie.poster_path}
+        alt={`Poster for ${currentMovie.title}`}
+      />
       {/* <p className="movie-info">{release_date}, {genres[1].name}, {runtime}</p>  */}
-      <p className="average-rating">{Math.floor(currentMovie.average_rating)}/10</p>
+      <p className="average-rating">
+        {Math.floor(currentMovie.average_rating)}/10
+      </p>
       <p className="tagline">{currentMovie.tagline}</p>
       <p className="overview">{currentMovie.overview}</p>
-      <p className="budget"><strong>Budget: {currentMovie.budget}</strong></p>
-      <p className="revenue"><strong>Revenue: {currentMovie.revenue}</strong></p>
-      <Link to={`/${currentMovie.id}`} className='back-btn'>◀ back</Link>
+      <p className="budget">
+        <strong>Budget: {currentMovie.budget}</strong>
+      </p>
+      <p className="revenue">
+        <strong>Revenue: {currentMovie.revenue}</strong>
+      </p>
+      <Link to={`/`} className="back-btn">
+        ◀ back
+      </Link>
     </section>
-  )
-}
+  );
+};
 
 export default MovieDetails;
