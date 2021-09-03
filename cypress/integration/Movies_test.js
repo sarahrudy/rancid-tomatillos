8a7b2b8c418fff62d58b902e680afbf672d6e5e5
Sarah <sarahrudy@gmail.com>
2021-09-02 18:57:05 -0600

    Add initial tests for Movies page

--- cypress/integration/Movies_test.js
+++ cypress/integration/Movies_test.js
@@ -1,9 +1,22 @@
 describe('Movies', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3001/');
-  });
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('cy.hash() - get the current URL hash', () => {
+    cy.hash()
+      .should('be.empty')
+  })
 
   it('should be able to visit the url', () => {
-    cy.get('h1').contains('RANCID TOMATILLOS');
-  });
-});
+    cy.url()
+      .should('eq', 'http://localhost:3000/')
+  })
+
+  it('should have a nav bar with a heading', () => {
+    cy.get('h1')
+      .contains('RANCID TOMATILLOS')
+  })
+
+  
+})
