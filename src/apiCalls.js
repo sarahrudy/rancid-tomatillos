fc9659ef3cf734555ab5a751603c487492f9da6e
Justin Anthony <justincanthony@gmail.com>
2021-09-03 11:48:21 -0600

    No change

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,11 @@
 const baseUrl = 'https://rancid-tomatillos.herokuapp.com/api/v2';
 
-export const getMovies = () => {
-  return fetch(baseUrl + '/movies').then((res) => res.json());
+export const getMovies = async () => {
+  const res = await fetch(baseUrl + '/movies');
+  return await res.json();
 };
 
-export const getSingleMovie = (id) => {
-  return fetch(baseUrl + `/movies/${id}`).then((res) => res.json());
+export const getSingleMovie = async (id) => {
+  const res = await fetch(baseUrl + `/movies/${id}`);
+  return await res.json();
 };
