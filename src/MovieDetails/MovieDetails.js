31599603d2c4624a5a829dd5417ffc5bedca48fa
Justin Anthony <justincanthony@gmail.com>
2021-09-07 03:06:55 -0600

    Remove unecessary elements and rules

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -15,58 +15,53 @@ const MovieDetails = ({ id }) => {
   }, [id]);
 
   return (
-    <section className="single-movie-container">
-      <div className="movie-details">
-        <div className="movie-image-fade" />
-        <img
-          className="movie-image"
-          src={currentMovie.backdrop_path}
-          alt={currentMovie.title}
-        />
-        <h1 className="movie-title">{currentMovie.title}</h1>
-        {currentMovie.tagline && (
-          <p className="tagline">{currentMovie.tagline}</p>
-        )}
-        {currentMovie.genres && (
-          <p className="genres">
-            <strong>Genres:</strong> {currentMovie.genres.join(' - ')}
-          </p>
-        )}
-        <p>Release Date: {currentMovie.release_date}</p>
-        <p className="average-rating">
-          <strong>Average Rating: </strong>
-          {Math.floor(currentMovie.average_rating)} out of 10
+    <section className="movie-details">
+      <div className="movie-image-fade" />
+      <img
+        className="movie-image"
+        src={currentMovie.backdrop_path}
+        alt={currentMovie.title}
+      />
+      <h1 className="movie-title">{currentMovie.title}</h1>
+      {currentMovie.tagline && (
+        <p className="tagline">{currentMovie.tagline}</p>
+      )}
+      {currentMovie.genres && (
+        <p className="genres">
+          <strong>Genres:</strong> {currentMovie.genres.join(' - ')}
         </p>
-        <p className="overview-title">
-          <strong>Overview</strong>
+      )}
+      <p>Release Date: {currentMovie.release_date}</p>
+      <p className="average-rating">
+        <strong>Average Rating: </strong>
+        {Math.floor(currentMovie.average_rating)} out of 10
+      </p>
+      <p className="overview-title">
+        <strong>Overview</strong>
+      </p>
+      <p className="overview">
+        <br />
+        <br />
+        {currentMovie.overview}
+      </p>
+      {currentMovie.budget > 0 && (
+        <p className="budget">
+          <strong>Budget:</strong> ${currentMovie.budget.toLocaleString()}
         </p>
-        <p className="overview">
-          <br />
-          <br />
-          {currentMovie.overview}
+      )}
+      {currentMovie.revenue > 0 && (
+        <p className="revenue">
+          <strong>Revenue: </strong>${currentMovie.revenue.toLocaleString()}
         </p>
-        {currentMovie.budget > 0 && (
-          <p className="budget">
-            <strong>Budget:</strong> ${currentMovie.budget.toLocaleString()}
-          </p>
-        )}
-        {currentMovie.revenue > 0 && (
-          <p className="revenue">
-            <strong>Revenue: </strong>${currentMovie.revenue.toLocaleString()}
-          </p>
-        )}
-        <p className="runtime">
-          <strong>Runtime: </strong> {currentMovie.runtime} Mins
-        </p>
-        <Link to={`/`} className="back-btn">
-          ◀ back
-        </Link>
-      </div>
+      )}
+      <p className="runtime">
+        <strong>Runtime: </strong> {currentMovie.runtime} Mins
+      </p>
+      <Link to={`/`} className="back-btn">
+        Go Back
+      </Link>
     </section>
   );
 };
 
 export default MovieDetails;
-
-// unused - styling option
-// style={{ backgroundImage: `url(${currentMovie.backdrop_path})` }}
